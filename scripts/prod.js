9e0abb54af013a577d0b9c56ba33a4e89850d50e
KOCTOLOM <kostiap19@gmail.com>
2025-04-13 04:17:55 +0300

    1.3.1

--- scripts/prod.js
+++
@@ -1,3 +0,0 @@
-Blocks.surgeSmelter.removeConsumer(ConsumeItems);
-var items = new ItemStack.with(Items.copper, 12, Items.lead, 16, Items.titanium, 8, Items.silicon, 12);
-Blocks.surgeSmelter.consumeItems(items);
