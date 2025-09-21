865dfad5db72bf472d86e39a78ec85e74cf01d2b
KOCTOLOM <kostiap19@gmail.com>
2025-09-21 10:42:15 +0300

    1.4.4

--- scripts/reconstructors/additiveReconstructor.js
+++ scripts/reconstructors/additiveReconstructor.js
@@ -1,11 +1,11 @@
 const script = require("reconstructorHandler")
 
 script.handleReconstructor(Blocks.additiveReconstructor, 
-	UnitTypes.dagger, ItemStack.with(Items.silicon, 50, Items.graphite, 70),
-	UnitTypes.nova, ItemStack.with(Items.silicon, 20, Items.graphite, 125),
-	UnitTypes.crawler, ItemStack.with(Items.silicon, 40, Items.graphite, 30),
-	UnitTypes.flare, ItemStack.with( Items.silicon, 40, Items.graphite, 30),
+	UnitTypes.dagger, ItemStack.with(Items.silicon, 60, Items.graphite, 70),
+	UnitTypes.nova, ItemStack.with(Items.silicon, 40, Items.graphite, 115),
+	UnitTypes.crawler, ItemStack.with(Items.silicon, 40, Items.graphite, 70),
+	UnitTypes.flare, ItemStack.with( Items.silicon, 40, Items.graphite, 50),
 	UnitTypes.mono, ItemStack.with( Items.silicon, 50, Items.graphite, 50),
-	UnitTypes.risso, ItemStack.with( Items.silicon, 60, Items.graphite, 30),
-	UnitTypes.retusa, ItemStack.with(Items.silicon, 30, Items.graphite, 50)
+	UnitTypes.risso, ItemStack.with( Items.silicon, 90, Items.graphite, 70),
+	UnitTypes.retusa, ItemStack.with(Items.silicon, 90, Items.graphite, 50)
 );
