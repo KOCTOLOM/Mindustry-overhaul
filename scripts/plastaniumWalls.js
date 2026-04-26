3a0f9e83a48e5e7d88da28db122b144365bc4f3c
KOCTOLOM <kostiap19@gmail.com>
2026-04-26 20:04:50 +0300

    1.4.63

--- scripts/plastaniumWalls.js
+++ scripts/plastaniumWalls.js
@@ -2,13 +2,23 @@ var plastWallBullet = extend(ShrapnelBulletType, {
 	damage: 130,
 	lifetime: 25,
 	width: 5,
-    serrations: 3,
+    serrations: 0,
 	length: 7,
-	ammoMultiplier: 0,
 	fromColor: Pal.plastaniumFront,
 	toColor: Pal.plastaniumFront,
 	hitColor: Pal.plastaniumFront,
 });
+
+var healBullet = extend(BasicBulletType, {
+	damage: 1,
+	absorbable:false,
+	hittable:false,
+	collidesTeam:true,
+	healPercent:100,
+	lifetime: 1,
+	width: 0
+});
+
 
 const ReinfPlastWall = extend(Wall, "reinf-plast-wall", {
     requirements: ItemStack.with(Items.plastanium, 10, Items.metaglass, 20, Items.surgeAlloy, 8),
@@ -67,3 +77,36 @@ ReinfPlastWallLarge.buildType = () => extend(Wall.WallBuild, ReinfPlastWallLarge
     }
 });
 
+const UnpWall = extend(Wall, "unpierce-wall", {
+    requirements: ItemStack.with(Items.plastanium, 10, Items.metaglass, 20, Items.surgeAlloy, 8),
+    health: 99999999,
+    armor:9999999,
+    insulated: true,
+    absorbLasers: true,
+    clipSize:8,
+    lightClipSize:8,
+    canPickup:false,
+    drawCracks:false,
+    targetable:false,
+    underBullets:false,
+    drawTeamOverlay:false,
+    forceDark:true,
+    buildVisibility: BuildVisibility.hidden
+});
+
+UnpWall.buildType = () => extend(Wall.WallBuild, UnpWall, {
+    cooldown: 0,
+    collision(bullet){
+        this.super$collision(bullet);
+
+        this.cooldown++;
+
+        if(this.cooldown == 1) {
+            for(let i = 0; i < 8; i++){
+                healBullet.create(this, this.x, this.y, (360 / 8) * i);
+            }
+            this.cooldown = 0;
+        }
+        return true
+    }
+});
